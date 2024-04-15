9398ad704dfca4f0b65ea2d30de9d32ae5531da1
icaroccaetano <icaroccaetano@gmail.com>
2024-04-15 07:58:30 -0300

    Add commnent to Menu method

--- Forca.Dominio/JogoForca.cs
+++ Forca.Dominio/JogoForca.cs
@@ -11,16 +11,23 @@ namespace Forca.Dominio
 
         public void Menu()
         {
-            int aux = 0;
-            bool ctrl = true;
-            int tema, pos = 0;
-            List<String> palavras = new List<String>();
-            Random rnd = new Random();
+            int aux = 0; // aux
+            bool ctrl = true; // aux to whiles
+            int tema; // theeme choosen 
+            int pos = 0; // we will use to run inside strings
+            List<String> palavras = new List<String>(); // where will we store the words of the csv file
+            Random rnd = new Random(); // used to pick a random word
 
             Console.WriteLine("Bem vindo a Forca!");
 
-            while (ctrl)
+            while (ctrl) // picking the value of tentatives
             {
+                /*
+                 * three options to attempts (int tentativas):
+                 * easy -> 7 attempts (1)
+                 * medium -> 6 attempts (2)
+                 * hard -> 5 attempts  (3)
+                 */
                 Console.WriteLine("Escolha uma dificuldade:");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(" 1 - Facil (7 tentativas)");
@@ -30,10 +37,9 @@ namespace Forca.Dominio
                 Console.WriteLine(" 3 - Dificil (5 tentativas)");
                 Console.ForegroundColor = ConsoleColor.White;
 
-
                 aux = int.Parse(Console.ReadLine());
 
-                if (aux == 1 || aux == 2 || aux == 3) ctrl = false;
+                if (aux == 1 || aux == 2 || aux == 3) ctrl = false; // Testing if the number of attemps entered is valid
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -42,15 +48,21 @@ namespace Forca.Dominio
                 }
             }
 
-            Console.ForegroundColor = ConsoleColor.White;
-            tentativas = aux;
+            tentativas = aux; // Assigning the value to the attribute
 
+            // resetting the auxiliares
             aux = 0;
             ctrl = true;
 
 
-            while (ctrl)
+            while (ctrl) //Picking the value of theeme
             {
+                /*
+                 * Three options to theeme (int tema):
+                 * Movies (1)
+                 * Cars (2)
+                 * Countries (3)  
+                 */
                 Console.WriteLine("Escolha um tema:");
                 Console.WriteLine(" 1 - Filmes");
                 Console.WriteLine(" 2 - Carros");
@@ -58,7 +70,7 @@ namespace Forca.Dominio
 
                 aux = int.Parse(Console.ReadLine());
 
-                if (aux == 1 || aux == 2 || aux == 3) ctrl = false;
+                if (aux == 1 || aux == 2 || aux == 3) ctrl = false; // Testing if the number entered is valid
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -67,10 +79,14 @@ namespace Forca.Dominio
                 }
             }
 
-            tema = aux - 1;
+            tema = aux - 1; // Assigning the value to the attribute (-1 because in the csv file movie is 0, 6cars 1 and countries 2)
 
-            var csvPath = @"C:\Users\USER\source\repos\Forca\Forca.Dominio\palavras.csv";
-            using (var reader = new StreamReader(csvPath))
+            // resetting the auxiliares
+            aux = 0;
+            ctrl = true;
+
+            var csvPath = @"C:\Users\USER\source\repos\Forca\Forca.Dominio\palavras.csv"; // reading the csv file and adding the infos into a list
+            using (var reader = new StreamReader(csvPath)) 
             {
                 while (reader.EndOfStream == false)
                 {
@@ -79,15 +95,16 @@ namespace Forca.Dominio
                 }
             }
 
-            ctrl = true;
-            while (ctrl)
+            // the list of strings named "palavras" has the word a ";" and the number of the theeme (0, 1 or 2)
+
+            while (ctrl) // choosing a random word
             {
                 pos = rnd.Next(0, palavras.Count);
-                if (palavras[pos][palavras[pos].Length - 1].ToString() == tema.ToString()) ctrl = false;
+                if (palavras[pos][palavras[pos].Length - 1].ToString() == tema.ToString()) ctrl = false; // if the randon word has the samen theeme choosen we go out of the loop
 
             }
 
-            palavra = palavras[pos];
+            palavra = palavras[pos]; //  Assigning the value to the attribute
         }
 
         public void Jogar ()
